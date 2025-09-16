7b768ef974cf32a0f5a1876d30b02b78086e4bd1
Luis Lavaire <llavaire01@gmail.com>
2025-09-16 17:36:06 -0600

    Add vol.c.

---
+++ vol.c
@@ -0,0 +1,23 @@
+#include <alsa/asoundlib.h>
+
+int main(int argc, char **argv) {
+    long vol;
+
+    snd_mixer_t *handle;
+    snd_mixer_elem_t *elem;
+    snd_mixer_selem_id_t *sid;
+    snd_mixer_open(&handle, 0);
+    snd_mixer_attach(handle, "default");
+    snd_mixer_selem_register(handle, 0, 0);
+    snd_mixer_load(handle);
+    snd_mixer_selem_id_malloc(&sid);
+    snd_mixer_selem_id_set_index(sid, 0);
+    snd_mixer_selem_id_set_name(sid, "Master");
+    elem = snd_mixer_find_selem(handle, sid);
+    snd_mixer_selem_get_playback_volume(elem, SND_MIXER_SCHN_FRONT_LEFT, &vol);
+
+    if (*argv[1] == '+') vol++;
+    if (*argv[1] == '-') vol--;
+
+    snd_mixer_selem_set_playback_volume_all(elem, vol);
+}
